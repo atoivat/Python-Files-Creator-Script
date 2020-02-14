b7f643e7ec6acb02d6e0461bb5bc2da1459e7c62
Otavio <otavio.leite.ufu@gmail.com>
2020-02-14 19:48:37 -0300

    First version

---
+++ py.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# ~ PYTHON FILES CREATOR SCRIPT ~
+# Put a name as first argument, and name.py will be created;
+# Otherwise, a automatic name will be generated based on time, and auto_name.py will be created.
+# The files always come with basic structure and permissions.
+
+_txt='#!/usr/bin/env python
+
+def main():
+	# Code goes here
+
+if __name__ == "__main__":
+   main()'
+
+
+if [ $# -gt "0" ]
+then
+	if [ -e $1.py ]
+	then
+   	echo "ERROR: A file named '$1.py' already exists."
+   	exit 1
+	fi
+	out=$1
+else
+	echo "Auto-creating..."
+	date=`date +"%d%m"`
+    out=$date
+    i=0
+    while [ -e $out.py ]
+    do
+        let i+=1
+        out="$date-$i"
+    done
+fi
+
+echo "$_txt" > $out.py
+chmod +x $out.py
+echo "$out.py created."
